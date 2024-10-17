3f33c1470cb6edd837d7f536ef91130bcb5fbb6b
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-17 16:26:55 -0400

    FINAL

--- university-query.sql
+++ university-query.sql
@@ -11,7 +11,7 @@ WHERE `cfu`> 10;
 --TODO 3- Selezionare tutti gli studenti che hanno più di 30 anni
 SELECT * 
 FROM `students` 
-WHERE YEAR(`date_of_birth`) <= 1994;
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
 
 --TODO 4- Selezionare tutti i corsi del primo semestre del primo anno di qualsiasi corso di laurea (286)
 SELECT * 
